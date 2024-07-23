da3a96b5d0ee401b1b303035354a2a94f7cffa58
TeLiNi <joaotelini03@gmail.com>
2024-07-23 18:06:36 -0300

    new ft

---
+++ js/app.js
@@ -0,0 +1,4 @@
+function boloWpp() {
+  window.location.assign("https://api.whatsapp.com/send/?phone=551991677323&text=Oii%2C+gostaria+de+fazer+um+pedido+com+voc%C3%AA+%EF%BF%BD&type=phone_number&app_absent=0");
+  console.log('Enviando mensagem')
+}
